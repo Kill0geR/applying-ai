4a41245385d87ee07f620d9535b4d3dc8882b064
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-09-28 21:33:05 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,92 +1,69 @@
-let autoscrolling = false;
-let lastScrollTop = 0;
-let timeoutId; // Variable to hold the timeout ID
+let timeoutId;
 let animating = false;
 const header = document.querySelector('header');
 header.classList.add('show-header');
-const sidebar = document.querySelector('.sidenav');
-document.addEventListener('DOMContentLoaded', function () {
-  // Get a reference to the hero image
-  const heroImage = document.querySelector('.hero-video img');
+let lastScrollTop = 0; // Declare lastScrollTop
 
-  // Get a reference to the scroll target element (the "Get Started" button)
+document.addEventListener('DOMContentLoaded', function () {
+  const heroImage = document.querySelector('.hero-video img');
   const scrollTarget = document.querySelector('.cta-button');
   animating = true;
-  // Calculate the scroll position to center the element vertically
   const scrollPosition = scrollTarget.getBoundingClientRect().top + window.scrollY - (window.innerHeight / 2);
 
-  // Add an event listener to the hero image for animationend
   heroImage.addEventListener('animationend', function () {
-    // Once the animation is finished, scroll to the calculated scroll position using smooth scrolling
     autoscrolling = true;
     animating = false;
     window.scrollTo({
       top: scrollPosition,
       behavior: 'smooth'
-
     });
-    clearTimeout(timeoutId); // Clear any previous timeout
+    clearTimeout(timeoutId);
     timeoutId = setTimeout(function () {
       autoscrolling = false;
-
     }, 1000);
   });
 });
 
-
-
-function scroll_to (target_id){
+function scroll_to(target_id) {
   const scrollTarget = document.querySelector(target_id);
   animating = true;
-  // Calculate the scroll position to center the element vertically
   const scrollPosition = scrollTarget.getBoundingClientRect().top + window.scrollY - (window.innerHeight / 2);
   window.scrollTo({
     top: scrollPosition,
     behavior: 'smooth'
-
   });
 }
-
 
 window.addEventListener('scroll', function () {
   const scrollTop = window.scrollY;
 
   if (scrollTop > lastScrollTop) {
     // Scrolling down, hide the header immediately
-    if(autoscrolling===false){
+    if (autoscrolling === false) {
       header.classList.remove('show-header');
-      sidebar.classList.add("show-header");
-
       clearTimeout(timeoutId); // Clear the timeout
-
     }
-
   } else {
     // Scrolling up or at the top, show the header
     header.classList.add('show-header');
-    sidebar.classList.remove("show-header");
 
     // Set a timeout to hide the header after 1 second
     clearTimeout(timeoutId); // Clear any previous timeout
     timeoutId = setTimeout(function () {
-
-      if(scrollTop <= 10){
-      } else{
-      header.classList.remove('show-header');
-      sidebar.classList.add("show-header");
-
-    }
-    }, 3000); // 1000 milliseconds = 1 second
+      if (scrollTop <= 10) {
+      } else {
+        header.classList.remove('show-header');
+      }
+    }, 1000); // 1000 milliseconds = 1 second
   }
   lastScrollTop = scrollTop;
 });
-window.onscroll = function() {myFunction()};
 
 function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  if(animating === false){
-  document.getElementById("myBar").style.height = scrolled + "%";
-}
+  if (animating === false) {
+    document.getElementById("myBar").style.height = scrolled + "%";
+  }
 }
