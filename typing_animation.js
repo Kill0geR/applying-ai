1d46a3d85612d5a89eccc4e8e5aa7544c6d977c5
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-09-29 23:20:11 +0200

    Add files via upload

---
+++ typing_animation.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".change_word", {
+    strings: ["APPLYING AI", "Finesse", "Innovation", "Professionalität", "Effizienz", "Raffinesse"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
