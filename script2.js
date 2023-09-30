fe402fe0e4d8512afc15e75a49fc4d909e3a5917
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-09-30 16:41:40 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,68 +1,16 @@
-let timeoutId;
-let animating = false;
-const header = document.querySelector('header');
-header.classList.add('show-header');
-let lastScrollTop = 0; // Declare lastScrollTop
+// JavaScript, um den Header beim Scrollen zu zeigen/verstecken
+let prevScrollPos = window.pageYOffset;
 
-document.addEventListener('DOMContentLoaded', function () {
-  const heroImage = document.querySelector('.hero-video img');
-  animating = true;
-  const scrollPosition = scrollTarget.getBoundingClientRect().top + window.scrollY - (window.innerHeight / 2);
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
 
-  heroImage.addEventListener('animationend', function () {
-    autoscrolling = true;
-    animating = false;
-    window.scrollTo({
-      top: scrollPosition,
-      behavior: 'smooth'
-    });
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(function () {
-      autoscrolling = false;
-    }, 1000);
-  });
-});
-
-function scroll_to(target_id) {
-  const scrollTarget = document.querySelector(target_id);
-  animating = true;
-  const scrollPosition = scrollTarget.getBoundingClientRect().top + window.scrollY - (window.innerHeight / 2);
-  window.scrollTo({
-    top: scrollPosition,
-    behavior: 'smooth'
-  });
-}
-
-window.addEventListener('scroll', function () {
-  const scrollTop = window.scrollY;
-
-  if (scrollTop > lastScrollTop) {
-    // Scrolling down, hide the header immediately
-    if (autoscrolling === false) {
-      header.classList.remove('show-header');
-      clearTimeout(timeoutId); // Clear the timeout
+    if (prevScrollPos > currentScrollPos) {
+        // Scrollt nach oben
+        document.querySelector("header").style.top = "0";
+    } else {
+        // Scrollt nach unten
+        document.querySelector("header").style.top = "-230px"; // Hier die Höhe deines Headers einsetzen
     }
-  } else {
-    // Scrolling up or at the top, show the header
-    header.classList.add('show-header');
 
-    // Set a timeout to hide the header after 1 second
-    clearTimeout(timeoutId); // Clear any previous timeout
-    timeoutId = setTimeout(function () {
-      if (scrollTop <= 10) {
-      } else {
-        header.classList.remove('show-header');
-      }
-    }, 1000); // 1000 milliseconds = 1 second
-  }
-  lastScrollTop = scrollTop;
-});
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  if (animating === false) {
-    document.getElementById("myBar").style.height = scrolled + "%";
-  }
+    prevScrollPos = currentScrollPos;
 }
