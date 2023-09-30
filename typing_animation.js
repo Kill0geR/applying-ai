d218c227bdb3ac19a6900e9e72286097b2c8cbb2
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-09-30 22:36:02 +0200

    Update typing_animation.js

--- typing_animation.js
+++ typing_animation.js
@@ -1,6 +1,6 @@
 var typed = new Typed(".change_word", {
     strings: ["APPLYING AI", "Finesse", "Innovation", "Professionalität", "Effizienz", "Raffinesse"],
-    typeSpeed: 150,
-    backSpeed: 150,
+    typeSpeed: 100,
+    backSpeed: 110,
     loop: true
 })
