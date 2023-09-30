a0bea129447f2187104bdff227e8577448f32da5
Fawaz <106278241+Kill0geR@users.noreply.github.com>
2023-09-30 16:44:25 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,16 +1,1 @@
-// JavaScript, um den Header beim Scrollen zu zeigen/verstecken
-let prevScrollPos = window.pageYOffset;
 
-window.onscroll = function() {
-    let currentScrollPos = window.pageYOffset;
-
-    if (prevScrollPos > currentScrollPos) {
-        // Scrollt nach oben
-        document.querySelector("header").style.top = "0";
-    } else {
-        // Scrollt nach unten
-        document.querySelector("header").style.top = "-230px"; // Hier die Höhe deines Headers einsetzen
-    }
-
-    prevScrollPos = currentScrollPos;
-}
